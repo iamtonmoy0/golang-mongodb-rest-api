791fd90c1e4f7acd45df7dc94304ea7692650116
iamtonmoy0 <edith@localhost.localdomain>
2022-12-30 22:50:23 +0600

    package imported and route added

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+func main() {
+
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:9000", r)
+}
+func getSession() *mgo.Session {
+	s, err := mgo.Dail("mongodb://localhost:27107")
+	if err != nil {
+		panic(err)
+	}
+	return s
+
+}
