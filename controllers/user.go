7cdace754a3c8b3d66e951fe192edac1a2228ded
iamtonmoy0 <tonmoy2code@gmail.com>
2022-12-31 13:52:48 +0600

    typing error solved

--- controllers/user.go
+++ controllers/user.go
@@ -53,13 +53,16 @@ func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ ht
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintf(w, "%s\n", uj)
 }
-func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Response, p httprouter.Params) {
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	id := p.ByName("id")
-	if !bson.IsObjectIdHex(id){
+	if !bson.IsObjectIdHex(id) {
 		w.WriteHeader(404)
 		return
 	}
 	oid := bson.ObjectIdHex(id)
-if err := uc.session.DB("mongo-golang").C("users").Remove(u)
-
+	if err := uc.session.DB("mongo-golang").C("users").RemoveId(oid); err != nil {
+		w.WriteHeader(404)
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Deleted user", oid, "\n")
 }
