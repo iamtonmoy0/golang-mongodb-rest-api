d3cdf909ff91e6d4f04f227cbde3ed9b4af9191f
iamtonmoy0 <edith@localhost.localdomain>
2022-12-31 12:30:59 +0600

    conrollers added

--- controllers/user.go
+++ controllers/user.go
@@ -1,1 +1,28 @@
 package controllers
+import(
+	"fmt"
+	"encoding/json"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"net/http"
+
+)
+type UserController struct{
+	session *mgo.Session
+}
+func NewUserController(s *mgo.Session) *UserController{
+	return &UserController{s}
+}
+
+func (uc UserController) GetUser (w http.ResponseWriter, r *http.Request ,p httprouter.params){
+	id := p.ByName("id")
+	if !bson.IsObjectIdHex(id){
+		w.WriteHeader(http.StatusNotFound)
+	}
+	oid := bson.ObjectIdHex(id)
+	u := models.User{}
+	if err := uc.session.DB("mongo.golang").C("users").FindId(oid).
+}
+CreateUser
+DeleteUser
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/iamtonmoy0/mongodb-rest-api/controllers"
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
 )
