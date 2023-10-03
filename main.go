545984316c363d25e6b2a78fc064640bd5bbe16a
iamtonmoy0 <tonmoy2code@gmail.com>
2023-10-03 14:55:50 -0400

    initial commit

--- main.go
+++ main.go
@@ -1,27 +1,7 @@
 package main
 
-import (
-	"net/http"
 
-	"github.com/iamtonmoy0/mongodb-rest-api/controllers"
-	"github.com/julienschmidt/httprouter"
-	"gopkg.in/mgo.v2"
-)
 
-func main() {
-
-	r := httprouter.New()
-	uc := controllers.NewUserController(getSession())
-	r.GET("/user/:id", uc.GetUser)
-	r.POST("/user", uc.CreateUser)
-	r.DELETE("/user/:id", uc.DeleteUser)
-	http.ListenAndServe("localhost:8000", r)
-}
-func getSession() *mgo.Session {
-	s, err := mgo.Dial("mongodb://localhost:27107")
-	if err != nil {
-		panic(err)
-	}
-	return s
-
+func main(){
+	
 }
