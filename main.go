b929a16e32de8b40d72e9c1fecdd6bc9e9b3f3f7
iamtonmoy0 <tonmoy2code@gmail.com>
2023-10-03 15:15:53 -0400

    feat:user model added and db connected

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
+import (
+	"os"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
 
 
 func main(){
-	
+	r:= httprouter.New()
+	uc:= controllers.NewUserController(getSession())
+	r.GET("/user/:id",uc.getUser)
+	r.POST("/user",uc.createUser)
+	r.DELETE("/user/:id",uc.deleteUser)
+
+}
+
+
+// get session
+func getSession() *mgo.Session{
+	dbURL:= os.Getenv("DB")
+s,err:=mgo.Dial(dbURL)
+if err!=nil{
+	panic("error connecting to db")
+}
+return s
 }
