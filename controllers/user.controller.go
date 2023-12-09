203a344b86d4888d78e203adbcf327040f2a6eb1
iamtonmoy0 <tonmoy2code@gmail.com>
2023-12-09 18:52:26 +0600

    create user added

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -42,4 +42,24 @@ func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httpr
 
 }
 
-//create user
+// create user
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request) {
+	u := models.User{}
+	json.NewDecoder(r.Body).Decode(&u)
+
+	u.Id = bson.NewObjectId()
+	uc.session.DB("golang-mongo").C("users").Insert(u)
+	uj, err := json.Marshal(u)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Println(w, "%s\n", uj)
+}
+
+// delete user
+
+func (uc.UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+}
